2e8ef31770cd5b6471a57f4e668202641c2fac36
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-05 12:43:27 -0500

    homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -257,7 +257,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o') {
+  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o'|| letra == 'u') {
     return 'es vocal';
   } else if (letra.lenght > 1){ 
     return 'dato incorrecto';
