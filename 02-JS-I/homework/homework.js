d77c2b6a2ddecefa669912ba7a6472049cae273d
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-05 12:10:59 -0500

    homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -193,9 +193,9 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   if (numero > 0) {
-    return 'es positivo' 
+    return 'Es positivo';
   } else {
-    return 'es negativo'
+    return 'Es negativo';
   }
 }
 
