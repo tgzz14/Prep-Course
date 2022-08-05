cc6e80f9f31afeba8fd9f5a4a83e5924587dae76
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-05 17:44:15 -0500

    homework 03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -47,6 +47,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma == 'aleman') {
+    return 'Guten tag!';
+  } else if (idioma == 'mandarin') {
+    return 'Ni Hao!';
+  } else if (idiona == ingles) {
+    return 'Hello!'
+  } else {
+    return 'Hola!';
+  }
 }
 
 function colors(color) {
