9a78c73d1fedd967ec5cda1f2620419e483c6b67
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-05 13:13:21 -0500

    homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -195,11 +195,11 @@ function esPositivo(numero) {
   
   if (numero === 0) {
     return false;
-  } else if (numero >0 ) {
+  } else if (numero > 0 ) {
     return 'es positivo';
-  } else {
-    return 'es negativo';
-  }
+  } 
+  return 'es negativo';
+  
 }
 
 function agregarSimboloExclamacion(str) {
@@ -260,14 +260,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.length > 1){
-    return "Dato incorrecto"
-  }
-  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-    return "Es vocal"
-  }
-  return "Dato incorrecto"
-
+if(letra.length > 1) {
+  return 'dato incorrecto';
+}
+if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+  return 'es vocal';
+} return 'dato incorrecto';
 }
 
 
