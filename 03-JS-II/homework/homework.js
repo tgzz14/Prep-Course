b24a46422503d99747c19213981e8abbdb6881ea
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-08 15:46:18 -0500

    homework 03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -163,8 +163,8 @@ function operadoresLogicos(num1, num2, num3) {
   if ( num1 > num2 && num1 > num3 && num1 > 0) {
     return 'Número 1 es mayor y positivo';
   } if (num1 < 0 || num2 < 0 || num3 < 0) {
-    return 'Hay negativos'
-  } if (num3 > num1 && num3 > n2) {
+    return 'Hay negativos';
+  } if (num3 > num1 && num3 > num2) {
     return num3 + 1;
   } if (num1 === 0 || num2 === 0 || num3 === 0) {
     return 'Error';
