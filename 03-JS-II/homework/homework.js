78ada62af553de2b924c89cb7e9aaff6aff03d7c
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-08 14:26:43 -0500

    homework 03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -93,6 +93,11 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero == 10 || numero == 5) {
+    return 'true';
+  } else {
+    return 'false';
+  }
 }
 
 function estaEnRango(numero) {
