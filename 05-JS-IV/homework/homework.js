c3771ec51045c3fe7a655cf67052a5743391926b
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-10 17:20:37 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,7 +6,7 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-  var obj = { 
+  var gato = { 
     nombre: 'nombre', 
     edad: 'edad',
     meow: function () {
