5f5259e9cd2a3552c284b908282c58a56f3ae5a8
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-10 10:31:28 -0500

    homework 03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -210,6 +210,9 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if ( numero < 99 && numero < 1000) {
+    return true;
+  } return false; 
   
 }
 
@@ -217,7 +220,12 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+var i = 0; 
+  while ( i < 8 ) {
+  numero = numero + 5; 
+  i++; 
 }
+} 
 
 
 // No modificar nada debajo de esta línea
