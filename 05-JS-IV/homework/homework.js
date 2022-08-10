2e814aa5ad70f62808084202a15d15cf5df1b966
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-10 17:46:08 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -22,7 +22,7 @@ function agregarPropiedad (objeto, property) {
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
   var objeto = {
-    property: null
+    property: 'null'
   }
    return objeto; 
 }
