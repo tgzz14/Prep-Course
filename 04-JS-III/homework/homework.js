886bb9693c566e0525fe8dac0c5d29fbc3440f2c
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-10 12:01:31 -0500

    homework 04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -28,10 +28,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-
+ nuevoArr = [];
   for (var i = 0 ; i < arraylength ; i++) {
-    array[i] = array[i] + 1; 
-  } return array; 
+    nuevoArr.push(array[i] + 1); 
+  } return nuevoArr; 
 }
 
 
