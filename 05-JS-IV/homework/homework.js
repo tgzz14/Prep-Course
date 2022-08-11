9a653aa1e2e5ad60d8297d9a0eccc69951b1cf0c
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-11 17:47:34 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -29,8 +29,9 @@ function invocarMetodo (objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
-  // Tu código:
-  return objeto.metodo(); 
+  // Tu código
+
+objeto.metodo(); 
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
