a1a1393b758ac2aa34a87b743f890151fb4a4044
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-12 12:00:26 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -23,7 +23,7 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto['property'] = 'null'; 
+  objeto['a'] = null; 
 
   return objeto; 
 
