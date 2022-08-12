6366f1ecd93397db282b1a3f17a09a764b353708
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-12 12:54:26 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -99,6 +99,11 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (usuario['password'] === password) {
+    return true;
+  } else {
+    return false; 
+  }
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
