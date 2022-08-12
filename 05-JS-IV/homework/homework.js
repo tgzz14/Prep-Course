4efb4a030136fb25accaa883794421645dedc901
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-12 11:52:02 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,7 +6,16 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato = { 
+    nombre : nombre,
+    edad : edad,
+    meow : function() { 
+      return 'Meow!';
+    } 
+
+  }
  
+   return gato; 
 }
 
 function agregarPropiedad (objeto, property) {
@@ -14,6 +23,9 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto['property'] = null; 
+
+  return objeto; 
 
 
   }
