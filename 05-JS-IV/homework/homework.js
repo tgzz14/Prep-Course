27a8ba47ed2a581365d0cf8d152991b06e388a02
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-12 12:14:54 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -23,7 +23,7 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto['a'] = null; 
+  objeto[property] = null; 
 
   return objeto; 
 
@@ -35,6 +35,7 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código
+  objeto.metodo(); 
 
 
 }
