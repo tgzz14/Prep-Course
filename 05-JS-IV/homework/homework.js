85721f3e93b184573732b6130e854baa70e169b1
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-12 13:48:24 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -131,6 +131,10 @@ function pasarUsuarioAPremium (usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for( var i = 0; i < usuarios.length; i++) {
+    usuarios[i].esPremium = true; 
+  }
+  return usuarios; 
 }
 
 function sumarLikesDeUsuario (usuario) {
