65250ea2d54d81230264096fbca6a7aaa03e8390
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-12 12:32:31 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -61,6 +61,13 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  var nuevoObj = {
+    nombre : nombr,
+    email : email,
+    password : password
+  }
+
+  return nuevoObj; 
 
 }
 
