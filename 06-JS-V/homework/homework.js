483fc29f0fe544c603b7d2d7fd1f7e4354ca9ca6
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-17 15:47:47 -0500

    homework 06

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -54,11 +54,20 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre, apellido, edad, domicilio) {
       // Crea el constructor:
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio
+    }
 
+    detalle() {
+      return maria;
     }
 }
+
+var maria = new Persona('Maria', 'Perez', 33, 'via casino');
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
