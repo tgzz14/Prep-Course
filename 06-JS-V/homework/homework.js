423a2f02b552c4eb0267dad286d734e310bbfbb0
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-17 15:24:51 -0500

    homework 06

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -26,6 +26,9 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  agregarMetodoPrototype.prototype.saludas = function() {
+    return 'Hello Word!';
+  }
 }
 
 function agregarStringInvertida() {
