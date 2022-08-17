99f7e42b539f14eca8cef7c950c6c720ec0ff0cd
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-17 11:11:54 -0500

    homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -162,7 +162,7 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  producto.calcularPrecioDescuento = function () {
+  producto.calcularPrecioDescuento = function() {
     return producto.precio - (producto.precio * producto.porcentajeDeDescuento)
   }
 
