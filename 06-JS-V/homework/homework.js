cd13f5d97c464d37faf27b100cee162be5faf51f
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-17 15:15:47 -0500

    homework 06

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,18 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  class Usuario {
+    constructor(usuario, nombre, email, password) {
+      this.usuario = usuario,
+      this.nombre = nombre,
+      this.email = email,
+      this.password = password
+    }
+    saludar() {
+      return 'Hola, mi nombre es ' + this.nombre;
+    }
+  }
+  return Usuario; 
 }
 
 function agregarMetodoPrototype(Constructor) {
