28d594148cffb119eafeac23f6fbef8859676e9c
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-18 16:42:49 -0500

    homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -40,6 +40,9 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento){
+   return  cb(elemento); 
+  })
 }
 
 function map(array, cb) {
