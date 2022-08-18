24a8da99b0201778f1c330f38eb6359f8a369740
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-18 17:21:55 -0500

    homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -49,7 +49,7 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  nuevoArr = array.map(function(elemento, indice){
+  nuevoArr = array.forEach(function(elemento){
         return cb(elemento);
   })
 }
