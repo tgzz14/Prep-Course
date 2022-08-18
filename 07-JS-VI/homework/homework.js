fe7262bde7452e03c4b1d2bdbe9d80bd643afe41
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-18 16:56:15 -0500

    homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,9 +4,10 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
- nombre = nombre.map(function(elemento){
+nombre.map(function(elemento){
     return elemento.capitalize(); 
   })
+  return nombre; 
 }
 
 function invocarCallback(cb) {
