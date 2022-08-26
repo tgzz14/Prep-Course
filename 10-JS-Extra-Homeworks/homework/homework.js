2d662b99cdd952cf9afe91f637abd5036def246f
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-26 13:48:36 -0500

    homework 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -82,6 +82,12 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  nuevoStr = '';
+  for (var i=0 ; i < cadena.length ; i++) {
+    if (cadena[i] !== 'a' || cadena[i] !== 'b' || cadena[i] !== 'c') {
+      nuevoStr += cadena[i];
+    }
+  } return nuevoStr; 
 }
 
 
