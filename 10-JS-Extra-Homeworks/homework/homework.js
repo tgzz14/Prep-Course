8373a9f40a026b3c0c976d188c98d2fd956cea72
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-26 16:32:08 -0500

    homework 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -85,10 +85,17 @@ function deleteAbc(cadena){
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
 
- var newStr = cadena.replace('abc', '');
-
-    return newStr; 
+  var a = [];
+  var arr = cadena.split('');
+  for (var i = 0; i < cadena.length; i++) {
+    if(cadena[i] !== "a"|| cadena[i] !== "b" || cadena[i] !== "c") {
+     a.push(cadena[i])
+     
+    }
     
+  }
+ return a.toString();
+
 }
 
 
