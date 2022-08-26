ef086c66c8472988b582f7ce017f6ce0fa114e43
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-08-26 13:44:30 -0500

    homework 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -66,6 +66,15 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var numeroStr = numero.toString();
+  var capicua = numeroStr.split('').reverse().join(''); 
+
+  if ( numero == capicua ) {
+    return 'Es capicua';
+  } else {
+    return 'No es capicua'; 
+  }
+
 }
 
 
